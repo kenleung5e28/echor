dedfa76c2fed30a4e00ff48999c0f9f06bccc22f
Ken C.Y. Leung <kenleung5e28@gmail.com>
2022-02-21 01:59:16 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -21,5 +21,5 @@ fn main() {
         .get_matches();
     let text = matches.values_of_lossy("text").unwrap();
     let omit_newline = matches.is_present("omit_newline");
-    print!("{}{}", text.join(" "), if omit_newline { " " } else { "\n" });
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 }
